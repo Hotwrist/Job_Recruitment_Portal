29fc072902a337bea33986d84c945f009120e978
Hot-wrist <odeyjhn@gmail.com>
2022-08-22 10:27:35 +0100

    Delete comfirm_reg_run.sh

--- register/comfirm_reg_run.sh
+++
@@ -1,3 +0,0 @@
-# Odey John Ebinyi aka Hotwrist
-# This bash script compiles the confirm_registration.cpp in this folder to confirm.cgi
-g++ -Wall -I/usr/include/cppconn  -I/usr/local/include/Poco -o confirm.cgi confirm_registration.cpp -L/usr/lib -lmysqlcppconn -lPocoNet -lcrypto -lcgicc -L/home/hot-wrist/Downloads/chilkat-9.5.0-x86_64-linux-gcc/lib -lchilkat-9.5.0 -lresolv -lpthread
